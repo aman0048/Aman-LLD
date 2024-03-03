9492d7293c31ba556572b30c0c74569b6390e751
aman0048 <jerrygarg16@gmail.com>
2024-03-03 14:11:35 +0530

    Executor Class Demo

---
+++ LLD-1/src/multiThreading/Executors/ExecutorDemo.java
@@ -0,0 +1,14 @@
+package multiThreading.Executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i<100; i++){
+            executorService.execute(new NumberPrinter(i));
+        }
+        executorService.shutdown();
+    }
+}
---
+++ LLD-1/src/multiThreading/Executors/Main.java
@@ -0,0 +1,11 @@
+package multiThreading.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i<100; i++){
+            Thread t = new Thread(new NumberPrinter(i));
+            t.start();
+        }
+    }
+}
