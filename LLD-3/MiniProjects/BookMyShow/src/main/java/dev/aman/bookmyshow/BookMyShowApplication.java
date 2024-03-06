aacc6f71ffd3c77e350446fc430c1561298ea57f
aman0048 <jerrygarg16@gmail.com>
2024-03-06 14:49:08 +0530

    Initialized new Book My show Project

--- LLD-3/MiniProjects/BookMyShow/src/main/java/dev/aman/bookmyshow/BookMyShowApplication.java
+++ LLD-3/MiniProjects/BookMyShow/src/main/java/dev/aman/bookmyshow/BookMyShowApplication.java
@@ -1,4 +1,4 @@
-package dev.aman.BookMyShow;
+package dev.aman.bookmyshow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- LLD-3/MiniProjects/BookMyShow/src/test/java/dev/aman/bookmyshow/BookMyShowApplicationTests.java
+++ LLD-3/MiniProjects/BookMyShow/src/test/java/dev/aman/bookmyshow/BookMyShowApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.aman.BookMyShow;
+package dev.aman.bookmyshow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
